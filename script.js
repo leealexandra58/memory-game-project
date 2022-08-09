b4e8c22811b3dd51b4babac9db55038d739bddb1
Sammy Erickson <0uts1d3th3b0x355@gmail.com>
2022-08-08 21:09:28 -0400

    start timer

--- script.js
+++ script.js
@@ -1,33 +1,21 @@
+let timer;
 
-const findMostCommon = function (array){
-    let totals = {};
-    for(const item of array){
-        if(totals[item]){
-            totals[item] += 1;
-        }else{
-            totals[item] = 1;
-        }
-    }
-    return totals;
-};
-
-document.getElementById("numberCards").addEventListener("change", (event) =>{
+document.getElementById("start").addEventListener("click", (event) => {
     event.preventDefault();
-    let numberOfCards = document.getElementById("numberCards").value;
-    let cardArray =[];
-    while(cardArray.length < numberOfCards){
-        r = Math.ceil(Math.random() * (numberOfCards / 2));
-        if(findMostCommon(cardArray)[r] === 2){
-            continue;
-        } else {
-            cardArray.push(r);
+   let timeElapsedsecond = 00;
+   let timeElapsedMin = 00;
+   let timeElapsedHour = 00;
+   timer = setInterval(()=> {
+        timeElapsedsecond ++;
+        
+        if(timeElapsedsecond === 60){
+            timeElapsedsecond = 0;
+            timeElapsedMin ++;
+            if(timeElapsedMin === 60){
+                timeElapsedMin = 0;
+                timeElapsedHour ++;
+            }
         }
-    }
-    for(i = 0; i < numberOfCards; i++){
-        let newDiv = document.createElement("div");
-        newDiv.classList.add("card");
-        newDiv.innerText = cardArray[i];
-        console.log(newDiv);
-        document.getElementById("cards").appendChild(newDiv);
-    }
+        document.getElementById("timer").innerText = `${timeElapsedHour}:${timeElapsedMin}:${timeElapsedsecond}`;
+   }, 1000);
 });
