4bfabfd93de6e6568a3489041d217d0411b0c3ef
Sammy Erickson <0uts1d3th3b0x355@gmail.com>
2022-08-10 18:44:38 -0400

    reset button

--- script.js
+++ script.js
@@ -1,33 +1,7 @@
 
-const findMostCommon = function (array){
-    let totals = {};
-    for(const item of array){
-        if(totals[item]){
-            totals[item] += 1;
-        }else{
-            totals[item] = 1;
-        }
-    }
-    return totals;
-};
-
-document.getElementById("numberCards").addEventListener("change", (event) =>{
+document.getElementById("reset").addEventListener("click", (event) => {
     event.preventDefault();
-    let numberOfCards = document.getElementById("numberCards").value;
-    let cardArray =[];
-    while(cardArray.length < numberOfCards){
-        r = Math.ceil(Math.random() * (numberOfCards / 2));
-        if(findMostCommon(cardArray)[r] === 2){
-            continue;
-        } else {
-            cardArray.push(r);
-        }
-    }
-    for(i = 0; i < numberOfCards; i++){
-        let newDiv = document.createElement("div");
-        newDiv.classList.add("card");
-        newDiv.innerText = cardArray[i];
-        console.log(newDiv);
-        document.getElementById("cards").appendChild(newDiv);
-    }
-});
+    clearBoard();
+    shuffle();
+    clearInterval(timer);
+})
